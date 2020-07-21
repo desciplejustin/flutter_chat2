453585bc4f06bef45026d8f28f49b26cf1e9522e
desciplejustin <desciplejustin@gmail.com>
2020-07-21 22:08:41 +0200

    Progress on tutorial

--- flutter_chat_ui_starter/lib/screens/home_screen.dart
+++ flutter_chat_ui_starter/lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui_starter/widgets/category_selector.dart';
+import 'package:flutter_chat_ui_starter/widgets/favorite_contacts.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,7 +11,54 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        title: Center(
+          child: Text(
+            "Chats",
+            style: TextStyle(
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+        ]
+      ),
+      body: Column(
+        children: <Widget> [
+          CategorySelector(),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(color: Theme.of(context).accentColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30.0),
+                topRight: Radius.circular(30.0),
+                ),
+              ),
+              child: Column(
+                children: <Widget>[
+                  FavoriteContacts(),
+                ]
+              )
+            ),
+          )
+        ]
+      )
     );
   }
 }
